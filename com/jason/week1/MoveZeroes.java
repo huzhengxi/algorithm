9b62fc153eb7e3686570b0920deb9c872ded14b0
Tiger <15264316179@163.com>
2021-03-11 10:06:56 +0800

    26. 删除排序数组中的重复项

--- com/jason/week1/MoveZeroes.java
+++ com/jason/week1/MoveZeroes.java
@@ -1,9 +1,14 @@
 
-/**
- * 283.移动零
- */
 public class MoveZeroes {
+    /**
+     * 83.移动零 时间复杂度 O(N) 空间复杂度 O(1)
+     * 
+     * @param nums 待操作数组
+     */
     public void moveZeroes(int[] nums) {
+        if (nums.length == 0) {
+            return;
+        }
         int j = 0; // 用来记录不为0的数字下标
         for (int i = 0; i < nums.length; i++) {
             // 不为0的时候才能操作
