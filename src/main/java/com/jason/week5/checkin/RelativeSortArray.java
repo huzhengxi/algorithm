da89bde158ab4937e8ca525c689cafb511423798
Tiger <15264316179@163.com>
2021-04-08 23:15:28 +0800

    * 1122. 数组的相对排序

--- src/main/java/com/jason/week5/checkin/RelativeSortArray.java
+++ src/main/java/com/jason/week5/checkin/RelativeSortArray.java
@@ -38,8 +38,8 @@ public class RelativeSortArray {
   }
 
   public static void main(String[] args) {
-    int[] arr1 = {2,3,1,3,2,4,6,7,9,2,19};
-    int[] arr2 = {2,1,4,3,9,6};
+    int[] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
+    int[] arr2 = {2, 1, 4, 3, 9, 6};
     RelativeSortArray relativeSortArray = new RelativeSortArray();
     Utils.printArray(relativeSortArray.relativeSortArray(arr1, arr2));
   }
