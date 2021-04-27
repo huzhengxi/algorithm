876cc8f56a76eb14e1475a8a4b706154e7bb658e
Tiger <15264316179@163.com>
2021-04-27 09:22:15 +0800

    * 15. 三数之和

--- src/main/java/com/jason/fourthtime/ThreeSum.java
+++ src/main/java/com/jason/fourthtime/ThreeSum.java
@@ -1,9 +1,8 @@
-package com.jason.thirdtime;
+package com.jason.fourthtime;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 
 /**
  * 15. 三数之和
@@ -14,43 +13,29 @@ import java.util.List;
  * <p>
  * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/3sum 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+
 public class ThreeSum {
 
-  /**
-   * 双指针法 时间复杂度O(n2)  空间复杂度O(logN)
-   *
-   * @param nums
-   * @return
-   */
   public List<List<Integer>> threeSum(int[] nums) {
     int len = nums.length;
-    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
     if (len <= 0) {
-      return ans;
+      return res;
     }
     Arrays.sort(nums);
-    //枚举a
     for (int first = 0; first < len; first++) {
-      //需要和上次枚举的数不相同
       if (first > 0 && nums[first] == nums[first - 1]) {
         continue;
       }
-
-      //c 对应的指针初始指向数组的最右端
       int third = len - 1;
-      //a + b + c = 0的话， b + c = -a，这里的b+c就是下面的target
       int target = -nums[first];
-      //枚举b
       for (int second = first + 1; second < len; second++) {
-        //需要和上次枚举的不相同
         if (second > first + 1 && nums[second] == nums[second - 1]) {
           continue;
         }
-        //需要保证b的指针在c的指针的左侧
         while (second < third && nums[second] + nums[third] > target) {
           --third;
         }
-        //如果指针重合，随着 b 后续的增加，就不会有满足 a + b +c = 0 并且 b < c 的 c 了，可以退出循环
         if (second == third) {
           break;
         }
@@ -59,18 +44,18 @@ public class ThreeSum {
           list.add(nums[first]);
           list.add(nums[second]);
           list.add(nums[third]);
-          ans.add(list);
+          res.add(list);
         }
       }
     }
-    return ans;
+    return res;
   }
 
 
   public static void main(String[] args) {
+    ThreeSum threeSum = new ThreeSum();
     int[] nums = {-1, 0, 1, 2, -1, -4};
-    ThreeSum sum = new ThreeSum();
-    List<List<Integer>> res = sum.threeSum(nums);
+    List<List<Integer>> res = threeSum.threeSum(nums);
     for (int i = 0; i < res.size(); i++) {
       List<Integer> list = res.get(i);
       for (int j = 0; j < list.size(); j++) {
